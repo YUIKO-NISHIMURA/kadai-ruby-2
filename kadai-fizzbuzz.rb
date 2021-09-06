e6358e344e75abe5907512221c85ad3860e36a51
yuiko.nishimura <yuzuko.1020@gmail.com>
2021-09-06 03:02:52 +0000

    final

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,18 @@
+num_max = 100
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0
+    puts "FizzBuzz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 3 == 0 
+    puts "Fizz"
+  else
+    puts num 
+  end 
+end
+
+(1..num_max).each do |number|
+  fizzbuzz(number)
+end 
+
+
